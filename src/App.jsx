c2796a05aed8d4fae023238c5bb3f2f99817cdd8
mahimakamat <mahimakamat8@gmail.com>
2026-06-25 14:01:28 +0530

    fix webrtc cross device

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,15 @@ export default function App() {
 
   const initPeerConnection = (socket) => {
     peerConnection.current = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-    });
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ],
+});
 
     // Send local ICE candidates to the remote peer
     peerConnection.current.onicecandidate = (event) => {
